e88d9449315bbc98e2832ed2dbf49cdabf214ccc
John Otene <johnotene1990@gmail.com>
2025-11-18 04:56:37 +0100

    update4

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,29 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { connectDB } from "./db/db.js";
 import cors from "cors";
-import { app, server } from "./db/socket.js";
+
 import path from "path";
+
+import { connectDB } from "./db/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./db/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
-const _dirname = path.resolve();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
+app.use(express.json());
 app.use(cookieParser());
-
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173"; // * for monorepo serving frontend
-app.use(cors({
- origin: CLIENT_URL,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -27,18 +31,15 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-
-const frontendPath = path.join(_dirname, "frontend/dist");
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(frontendPath));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-
 server.listen(PORT, () => {
-  console.log("✅ Server is running on PORT: " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
